1e8de493f2910d4967ee4b876027b4bda38287a6
Anju062003 <146409476+Anju062003@users.noreply.github.com>
2025-04-24 11:10:48 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,11 +1,14 @@
 provider "aws" {
-    region = "us-east-1"  
+  alias  = "ec2"
+  region = "ap-south-1"
 }
+resource "aws_instance" "my_instance" {
+  provider = aws.ec2
 
-resource "aws_instance" "foo" {
-  ami           = "ami-05fa00d4c63e32376" # us-west-2
+  ami           = "ami-0f1dcc636b69a6438"  # Use your actual AMI
   instance_type = "t2.micro"
+
   tags = {
-      Name = "TF-Instance"
+    Name = "Anju-Terraform-Instance"
   }
 }
